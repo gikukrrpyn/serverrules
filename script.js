ef06608186016abd13dd13d0cddbc613c1a7513a
giknikyev <ukrainerpeh@gmail.com>
2025-06-05 00:42:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -346,11 +346,14 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-
 function getTodayKey() {
-  const date = new Date().toISOString().split('T')[0];
-  return `views/${date}`;
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `views/${year}-${month}-${day}`;
 }
+
 
 function incrementGlobalViewCounter() {
   const ref = firebase.database().ref(getTodayKey());
