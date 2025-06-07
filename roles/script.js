e9350647773c2b4cfbdf969723d5eeda9e1af0d1
giknikyev <ukrainerpeh@gmail.com>
2025-06-07 15:24:09 +0300

    Update script.js

--- roles/script.js
+++ roles/script.js
@@ -25,6 +25,7 @@
   "Tyjorty123987": { role: "Співробітник СБС", telegram: "1410588546", category: "СБС" },
   "Black_Batman02": { role: "Співробітник СБС", telegram: "1093245097", category: "СБС" },
   "maksumkasymk789": { role: "Співробітник СБС", telegram: "1405483524", category: "СБС" },
+  "Ksjhfte": { role: "Співробітник СБС", telegram: "7729164856", category: "СБС" },
 
                 "Vaylet009": { role: "Директор ДБР", telegram: "t.me/Vaylet009", category: "ДБР" },
                 "global20207": { role: "Заступник Директора ДБР", telegram: "1431122546", category: "ДБР" },
