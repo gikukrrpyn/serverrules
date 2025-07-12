c7c97c4b473276ac0c383300dfaff8244e83b224
giknikyev <ukrainerpeh@gmail.com>
2025-07-12 04:17:13 +0300

    Create marriedlist.js

---
+++ marriage/marriedlist.js
@@ -0,0 +1,31 @@
+export const marriedList = [
+   {
+        username1: "Trix30303",
+        username2: "34436euf",
+        date: "2025-22-06", 
+        photo1: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-F4E257BB13870A53DB6E3D24FC1D4D65-Png/150/150/AvatarHeadshot/Webp/noFilter",
+        photo2: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-2788EEE7304E0CBB391A8D0CB1780C9E-Png/150/150/AvatarHeadshot/Webp/noFilter"
+   },
+      {
+        username1: "Pol_ina12311",
+        username2: "stasik666667",
+        date: "2025-23-06", 
+        photo1: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-D3C698633B92A2308F198AF86639809F-Png/150/150/AvatarHeadshot/Webp/noFilter",
+        photo2: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-6F3930BE709B255A8F6D6F428607C023-Png/150/150/AvatarHeadshot/Webp/noFilter"
+   },
+
+         {
+        username1: "Junius_2008",
+        username2: "va1ron4",
+        date: "2025-26-06", 
+        photo1: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-9232DE9306618B5D071B2DC605CAB647-Png/150/150/AvatarHeadshot/Webp/noFilter",
+        photo2: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-6586EA63EC61E557F59731CEA1639F83-Png/150/150/AvatarHeadshot/Webp/noFilter"
+   },
+         {
+        username1: "bandera909",
+        username2: "starikashka228337",
+        date: "2025-28-06", 
+        photo1: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-F42F0593E4666C0263AB23BF8D330C91-Png/150/150/AvatarHeadshot/Webp/noFilter",
+        photo2: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-75304F4FDE48DF7811017948208C1ED7-Png/150/150/AvatarHeadshot/Webp/noFilter"
+   },
+  ];
