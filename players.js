5a17edf9ef44dc9c59cea2a8865ebe3b9cc00afc
gikukrrpyn <ukrainerpeh@gmail.com>
2025-10-03 01:07:58 +0300

    Update players.js

--- players.js
+++ players.js
@@ -19,14 +19,7 @@ const players = {
       avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-893F6FD6D7EF0F7B58E6BB6900E97E18-Png/150/150/AvatarHeadshot/Webp/noFilter",
     },
   
-   {
-      role: "Головний Суддя",
-      telegram: "t.me/Na7atelega",
-      category: "Суд",
-      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-893F6FD6D7EF0F7B58E6BB6900E97E18-Png/150/150/AvatarHeadshot/Webp/noFilter",
-    },
-
-        {
+     {
       role: "Контентмейкер",
       telegram: "t.me/Na7atelega",
       category: "Інше",
@@ -43,7 +36,7 @@ const players = {
 
 "Bandera909": [
     {
-      role: "R3 Старший Адміністратор + Адміністратор-Івентник",
+      role: "Головний адміністратор + Адміністратор-Івентник",
       telegram: "t.me/Great_Terku",
       category: "Адміністрація",
       avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-57527754C944AE793A07800781358CAE-Png/150/150/AvatarHeadshot/Webp/noFilter",
@@ -59,7 +52,7 @@ const players = {
 
 "H8TpizzaG8D": [
     {
-      role: "R1c Адміністратор-Комунікатор + Інспектор Підтримки",
+      role: "Адміністратор-Комунікатор + Інспектор Підтримки",
       telegram: "t.me/P1zzaGod",
       category: "Адміністрація",
       avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-BBF6A08B276D1D8E94206E9EB06933F9-Png/150/150/AvatarHeadshot/Webp/noFilter",
@@ -68,7 +61,7 @@ const players = {
 
 "Ntinetter": [
     {
-      role: "R1a Адміністратор-Патрульний",
+      role: "Адміністратор-Патрульний",
       telegram: "t.me/makyas10",
       category: "Адміністрація",
       avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-F1EF271A48F0ED1FD4C2732CF5F1F55E-Png/150/150/AvatarHeadshot/Webp/noFilter",
@@ -77,7 +70,7 @@ const players = {
 
     "Andrew_UR01": [
     {
-      role: "Суддя",
+      role: "Головний суддя",
       telegram: "t.me/ami_woolw",
       category: "Суд",
       avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-CB76B667D48AFE9E5B853A0EE8B923AD-Png/150/150/AvatarHeadshot/Webp/noFilter",
@@ -100,16 +93,6 @@ const players = {
       telegram: "t.me/Absolute_WT",
       category: "Мерія",
       avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-4A73371856D536748B49625E0B515AC2-Png/150/150/AvatarHeadshot/Webp/noFilter",
-    },
-  ],
-
-
- "Rostyk_gej71": [
-    {
-      role: "Водій лимузину",
-      telegram: "t.me/Kotro71",
-      category: "ДАРШ",
-      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-11B619988EB2E8536860B1784041BC09-Png/150/150/AvatarHeadshot/Webp/noFilter",
     },
   ],
 
