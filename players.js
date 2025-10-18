2c6743335e5dd3fce911d2d526382959ba909fa0
gikukrrpyn <ukrainerpeh@gmail.com>
2025-10-18 16:00:32 +0300

    Update players.js

--- players.js
+++ players.js
@@ -81,6 +81,70 @@ const players = {
     },
   ],
 
+  
+  "artemMelnichenko2013": [
+    {
+      role: "Працівник ДБР",
+      telegram: "5242336088",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-40C0C0541A9CA426461D9F9734328806-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+
+    "starikashka228337": [
+    {
+      role: "Працівник ДБР",
+      telegram: "5358641921",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-2331B1E6EF04F726C33C255BC1FEA9AA-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+
+      "maksumkasymk789": [
+    {
+      role: "Працівник ДБР",
+      telegram: "1405483524",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-1A96C6486ED87233BC622C2AB85BA72C-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+
+        "Dimabarbos": [
+    {
+      role: "Працівник ДБР",
+      telegram: "6591557304",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-A9106E381B813BC1AABA06EA684A037B-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+
+          "Arsenic3996": [
+    {
+      role: "Працівник ДБР",
+      telegram: "7643022641",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-81A7C1D70E500D3B8FCF82768C7849D1-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+
+            "eldor830": [
+    {
+      role: "Працівник ДБР",
+      telegram: "6267044472",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-E65399E363006B14297BD9FC893EC3FB-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+
+              "nikita100808": [
+    {
+      role: "Працівник ДБР",
+      telegram: "6348110356",
+      category: "ДБР",
+      avatar: "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-52426F10465A4F65F7E5105821318D01-Png/150/150/AvatarHeadshot/Webp/noFilter",
+    },
+  ],
+  
 "Absolute09_SO": [
     {
       role: "Мер",
