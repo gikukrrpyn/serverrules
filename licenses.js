bedd255ea7fdbe2b597b531825922f80db6e3fb5
EzDevPy <samtarpro@gmail.com>
2026-07-27 00:43:52 +0300

    Update licenses.js by ob1zyan (Role: Технічній адміністратор)

--- licenses.js
+++ licenses.js
@@ -20,7 +20,6 @@ window.licenses = {
     // M
     "Maksimka20131411": {"status":"Дійсна","expiry":"17.08.2026","telegram":"8556942531"},
     "makspsava": {"status":"Дійсна","expiry":"24.07.2026","telegram":"t.me/RoksyxMaks"},
-    "Mativiti_aaa": {"status":"Дійсна","expiry":"24.07.2026","telegram":"t.me/Matvei_Lego0_0"},
     "mativiti_aaa": {"status":"Дійсна","expiry":"24.08.2026","telegram":"t.me/Matvei_Lego0_0"},
     "Mykhailo_Zaya1": {"status":"Дійсна","expiry":"30.07.2026","telegram":"5295879849"},
 
