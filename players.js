1005a6788588546823665d480457b284d50e9296
EzDevPy <samtarpro@gmail.com>
2026-07-27 21:31:27 +0300

    Update players.js by jehfirgrivbrhtklf (Role: Начальник НПС)

--- players.js
+++ players.js
@@ -331,12 +331,6 @@ const players = {
             "avatar": ""
         },
         {
-            "username": "CAXAPOK_5685rb",
-            "role": "Капітан",
-            "telegram": "8852170844",
-            "avatar": ""
-        },
-        {
             "username": "Val1nOtm",
             "role": "Ветеран",
             "telegram": "@g0gocat",
