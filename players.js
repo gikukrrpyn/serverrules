e556083aa54ac44704b29fefbf985d70133f1220
EzDevPy <samtarpro@gmail.com>
2026-08-09 10:54:28 +0300

    Update players.js by dariysmertnk (Role: Заступник Директора СБС)

--- players.js
+++ players.js
@@ -109,6 +109,12 @@ const players = {
             "role": "Працівник СБС",
             "telegram": "@zefirpvp",
             "avatar": "https://i.postimg.cc/6qG4hb5h/Screenshot-2026-07-30-10-20-46-270-com-roblox-client-edit.jpg"
+        },
+        {
+            "username": "artemMelnichenko2013",
+            "role": "Директор СБС",
+            "telegram": "@hosters_full",
+            "avatar": "https://i.postimg.cc/SNvjychZ/Screenshot-2026-08-09-10-51-58-550-com-roblox-client-edit.jpg"
         }
     ],
     "ДБР": [
