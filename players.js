c24335cc2bab13b90dc94156c07f8cbcc5a755f1
EzDevPy <samtarpro@gmail.com>
2026-08-14 13:11:40 +0300

    Update players.js by ob1zyan (Role: Технічній адміністратор)

--- players.js
+++ players.js
@@ -279,12 +279,6 @@ const players = {
             "role": "Начальник НПС",
             "telegram": "t.me/Alexandriulik",
             "avatar": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-1290420954AF9DDF74699118861FCD63-Png/150/150/AvatarHeadshot/Png/noFilter"
-        },
-        {
-            "username": "Maksmakus",
-            "role": "Керівник ДП",
-            "telegram": "t.me/Maksikus19",
-            "avatar": "https://tr.rbxcdn.com/30DAY-AvatarHeadshot-86EC8AD5E099209C81C822DA9B8DCFFD-Png/150/150/AvatarHeadshot/Png/noFilter"
         }
     ],
     "МВС": [
